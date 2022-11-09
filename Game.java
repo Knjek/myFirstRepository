3802eff6c76eae717ae590a43f963c1e0b9ea69c
Adrian Åsling Sellius <adrian@gmail.com>
2022-11-09 10:39:46 +0100

    read countries from input

--- Game.java
+++ Game.java
@@ -1,15 +1,32 @@
 import java.io.Console;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 
 public class Game {
     public static void main(String[] args) {
-	System.out.println("Welcome to the Countries game.");
+	System.out.println("Countries Game");
 	Console console = System.console();
-	String answer = console.readLine("Which continent does Sweden belong to? ");
-	String continent = "Asia";
-	if (answer.equalsIgnoreCase(continent)) {
-		System.out.println("Correct!");
-	} else {
-		System.out.println("Wrong!");
+	BufferedReader br = null;
+	try {
+	    br = new BufferedReader(new FileReader("continents.csv"));
+	    String line;
+	    while((line=br.readLine()) != null) {
+		String[] cols = line.split(",");
+		String country = cols[0];
+		String continent= cols[1];
+		String answer= console.readLine("which continent does " + country+ " belong to?");
+	if(answer.equalsIgnoreCase(continent)) {
+	    System.out.println("Correct!");
+	}else {
+	    System.out.println("No, the answer is " + continent);
+	}
+    }
+	}catch(FileNotFoundException e) {
+	    e.printStackTrace();
+	}catch(IOException e) {
+	    e.printStackTrace();
 	}
     }
 }
